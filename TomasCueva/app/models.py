f6f170b7a86f45aa38f9fd9d618e13de51c5c046
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2023-04-26 11:39:24 -0300

    Cambio de colores, añadir responsividad a las cards, diseñar seccion de repositorios

--- TomasCueva/app/models.py
+++ TomasCueva/app/models.py
@@ -4,6 +4,8 @@ class Projects(models.Model):
     title = models.CharField(max_length=140)
     content = models.TextField(max_length=2600)
     image = models.ImageField(upload_to='projects_image')
+    github = models.URLField(max_length=200, blank=True,null=True,default=None)
+    view = models.URLField(max_length=200, blank=True,null=True,default=None)
     def __str__(self) -> str:
         return super().__str__()
 
--- TomasCueva/app/views.py
+++ TomasCueva/app/views.py
@@ -3,6 +3,26 @@ from .forms import *
 from .models import *
 from datetime import datetime
 from django.views.generic import ListView 
+import json
+import requests
+def repositorio_github(request):
+    url_api = "https://api.github.com/repos/{usuario}/{repositorio}/contents/{ruta_archivo}"
+    usuario = "CuevaTomasArg"
+    repositorio = "nombre_de_repositorio"
+    ruta_archivo = "ruta/del/archivo/en/el/repositorio"
+    token = "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"
+
+    headers = {
+        "Authorization": f"Token {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    respuesta_api = requests.get(url_api.format(usuario=usuario, repositorio=repositorio, ruta_archivo=ruta_archivo), headers=headers)
+
+    contenido = json.loads(respuesta_api.content)
+
+    return render(request, 'nombre_de_la_plantilla.html', {'contenido': contenido})
+
 
 class Index(ListView):
     model = Projects
